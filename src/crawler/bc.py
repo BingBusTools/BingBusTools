ca1229753b9c27d8f449d5533c23892b458299c6
niooii <wangh3@bxscience.edu>
2024-09-21 19:43:43 -0400

    trip updates

--- src/crawler/bc.py
+++ src/crawler/bc.py
@@ -14,7 +14,7 @@ class BCTransit:
     def __init__(self):
         self.session = aiohttp.ClientSession()
         self.vehicle_positions: FeedMessage = FeedMessage()
-        self.trip_update: FeedMessage = FeedMessage()
+        self.trip_updates: FeedMessage = FeedMessage()
         self.alert: FeedMessage = FeedMessage()
 
     def close(self) -> None:
@@ -25,14 +25,15 @@ class BCTransit:
             raise RuntimeError(f"BCApi: http request failed: {res.url}")
     
     def __vehicle_json_to_pb(self, vjson: Dict[str, str], out_entity: FeedEntity):
-            out_entity.id = str(vjson["id"])
-            out_entity.vehicle.position.latitude = float(vjson["lat"])
-            out_entity.vehicle.position.longitude = float(vjson["lon"])
+        out_entity.id = str(vjson["id"])
+        out_entity.vehicle.position.latitude = float(vjson["lat"])
+        out_entity.vehicle.position.longitude = float(vjson["lon"])
     
-    def __(self, vjson: Dict[str, str], out_entity: FeedEntity):
-            out_entity.id = str(vjson["id"])
-            out_entity.vehicle.position.latitude = float(vjson["lat"])
-            out_entity.vehicle.position.longitude = float(vjson["lon"])
+    def __trip_json_to_pb(self, route_id: str, trip_json: Dict[str, str], out_entity: FeedEntity):
+        out_entity.id = str(trip_json["id"])
+        out_entity.trip_update.trip.trip_id = str(trip_json["id"])
+        out_entity.trip_update.trip.route_id = route_id
+        out_entity.trip_update.trip.start_time = str(trip_json["start_time"])
     
     def update_vehicle_pos_feed(self, json_arr: Dict[str, str]):
         msg = self.vehicle_positions
@@ -40,25 +41,27 @@ class BCTransit:
 
         msg.header.gtfs_realtime_version = "2.0"
         msg.header.incrementality = FeedHeader.Incrementality.FULL_DATASET
-        # # TODO: do better
+        # TODO: do better
         msg.header.timestamp = int(time.time())
 
         for json in json_arr:
             ent = msg.entity.add()
             self.__vehicle_json_to_pb(json, ent)
     
-    def update_trip_update_feed(self, json_arr: Dict[str, str]):
-        msg = self.vehicle_positions
+    def update_trip_update_feed(self, data_json: Dict[str, str]):
+        msg = self.trip_updates
         msg.Clear()
 
         msg.header.gtfs_realtime_version = "2.0"
         msg.header.incrementality = FeedHeader.Incrementality.FULL_DATASET
-        # # TODO: do better
+        # TODO: do better
         msg.header.timestamp = int(time.time())
 
-        for json in json_arr:
+        for route_json in data_json["routes"]:
+            id: str = str(route_json["id"])
             ent = msg.entity.add()
-            self.__vehicle_json_to_pb(json, ent)
+            for trip_json in route_json["trips"]:
+                self.__trip_json_to_pb(id, trip_json, ent)
 
     def get_vehicle_positions(self) -> Union[str, None]:
         try:
@@ -67,8 +70,12 @@ class BCTransit:
             print(e)
             return None
 
-    def get_trip_updates() -> FeedMessage:
-        pass
+    def get_trip_updates(self) -> Union[str, None]:
+        try:
+            return self.trip_updates.SerializeToString()
+        except Exception as e:
+            print(e)
+            return None
 
     def get_alerts() -> FeedMessage:
         pass
@@ -87,6 +94,7 @@ class BCTransit:
             endpoints.BC_BUSES,
             endpoints.BC_ROUTES,
             endpoints.BC_STOPS,
+            endpoints.BC_TRIPS
         ]
 
         async def fetch(url: str) -> Dict[str, str]:
@@ -97,18 +105,20 @@ class BCTransit:
             *[fetch(url) for url in endpoint_list], return_exceptions=True
         )
 
-        buses_json = json_list[0]
-        routes_json = json_list[1]
-        stops_json = json_list[2]
+        buses_json: Dict[str, str] = json_list[0]
+        routes_json: Dict[str, str] = json_list[1]
+        stops_json: Dict[str, str] = json_list[2]
+        trips_json: Dict[str, str] = json_list[3]["data"]
 
         self.update_vehicle_pos_feed(buses_json)
+        self.update_trip_update_feed(trips_json)
 
 
 async def test_main() -> None:
     try:
         bc_wrapper: BCTransit = BCTransit()
         await bc_wrapper.update()
-        vehicle_positions: str = bc_wrapper.get_vehicle_positions()
+        vehicle_positions: str = bc_wrapper.get_trip_updates()
         print(vehicle_positions)
     except Exception as e:
         print(f"Error: {e}")
--- src/crawler/endpoints.py
+++ src/crawler/endpoints.py
@@ -1,6 +1,7 @@
 BC_ROUTES = "https://bctransit.doublemap.com/map/v2/routes"
 BC_BUSES = "https://bctransit.doublemap.com/map/v2/buses"
 BC_STOPS = "https://bctransit.doublemap.com/map/v2/stops"
+BC_TRIPS = "https://bctransit.doublemap.com/map/v2/trips"
 OCCT_VEHICLES = "https://binghamtonupublic.etaspot.net/service.php?service=get_vehicles&token=TESTING"
 OCCT_SERVICE_ANNOUNCEMENTS = "https://binghamtonupublic.etaspot.net/service.php?service=get_service_announcements&token=TESTING"
 OCCT_ROUTES = (
