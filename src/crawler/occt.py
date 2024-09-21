2cde2cc55b42a3d9082164f8357d0b0f0d0691f6
warfarm <danz32321@gmail.com>
2024-09-21 14:34:58 -0400

    OCCT session close

--- src/crawler/occt.py
+++ src/crawler/occt.py
@@ -6,10 +6,18 @@ import json
 
 class OCCT:
     def __init__(self):
+        self.session = aiohttp.ClientSession()
         self.VehicleData = {}
         self.RouteData = {}
         self.ServiceData = {}
         pass
+
+    def close(self) -> None:
+        return self.session.close()
+
+    def validate_res_status(self, res):
+        if res.status // 100 != 2:
+            raise RuntimeError(f"BCApi: http request failed: {res.url}")
 
     def checkVehicles(self, vehicleDict):
         for i, vehicle in enumerate(vehicleDict):
@@ -19,28 +27,25 @@ class OCCT:
         return vehicleDict
 
     async def pollVehicles(self) -> None:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(endpoints.OCCT_VEHICLES) as vehicleResponse:
-                jsonOCCTVEHICLES = await vehicleResponse.json()
-                self.VehicleData = self.checkVehicles(jsonOCCTVEHICLES["get_vehicles"])
-                # print(self.VehicleData)
+        async with self.session.get(endpoints.OCCT_VEHICLES) as vehicleResponse:
+            jsonOCCTVEHICLES = await vehicleResponse.json()
+            self.VehicleData = self.checkVehicles(jsonOCCTVEHICLES["get_vehicles"])
+            # print(self.VehicleData)
         return self.VehicleData
 
     async def pollRoutes(self) -> None:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(endpoints.OCCT_ROUTES) as routeResponse:
-                jsonOCCTRoutes = await routeResponse.json()
-                self.RouteData = jsonOCCTRoutes["get_routes"]
+        async with self.session.get(endpoints.OCCT_ROUTES) as routeResponse:
+            jsonOCCTRoutes = await routeResponse.json()
+            self.RouteData = jsonOCCTRoutes["get_routes"]
 
         return self.RouteData
 
     async def pollService(self) -> None:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                endpoints.OCCT_SERVICE_ANNOUNCEMENTS
-            ) as serviceResponse:
-                jsonOCCTService = await serviceResponse.json()
-                self.ServiceData = jsonOCCTService["get_service_announcements"]
+        async with self.session.get(
+            endpoints.OCCT_SERVICE_ANNOUNCEMENTS
+        ) as serviceResponse:
+            jsonOCCTService = await serviceResponse.json()
+            self.ServiceData = jsonOCCTService["get_service_announcements"]
 
         return self.ServiceData
 
@@ -51,8 +56,13 @@ class OCCT:
 
 
 async def main() -> None:
-    occt_wrapper: OCCT = OCCT()
-    await occt_wrapper.pollAll()
+    try:
+        occt_wrapper: OCCT = OCCT()
+        await occt_wrapper.pollAll()
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        await occt_wrapper.close()
 
 
 if __name__ == "__main__":
