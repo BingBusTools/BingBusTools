e865505eb15fb3f843f48bfd27a8300f0721ab60
niooii <wangh3@bxscience.edu>
2024-09-21 13:08:19 -0400

    add endpoints

---
+++ src/crawler/endpoints.py
@@ -0,0 +1,5 @@
+BC_ROUTES = "https://bctransit.doublemap.com/map/v2/routes"
+BC_BUSES = "https://bctransit.doublemap.com/map/v2/buses"
+BC_STOPS = "https://bctransit.doublemap.com/map/v2/stops"
+OCCT_VEHICLES = "https://binghamtonupublic.etaspot.net/service.php?service=get_vehicles&token=TESTING"
+OCCT_SERVICE_ANNOUNCEMENTS = "https://binghamtonupublic.etaspot.net/service.php?service=get_service_announcements&token=TESTING"
