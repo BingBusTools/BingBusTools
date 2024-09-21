ff17c0cfa9f7038896393d90f35a46a46ae75948
niooii <wangh3@bxscience.edu>
2024-09-21 16:08:03 -0400

    separate updating data and accessing

--- src/crawler/bc.py
+++ src/crawler/bc.py
@@ -2,10 +2,17 @@ import asyncio
 import endpoints
 import aiohttp
 from typing import Dict
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'protobuf')))
+import gtfs_realtime_pb2 as gtfsrt
 
-class BCApi:
+class BCTransit:
     def __init__(self):
         self.session = aiohttp.ClientSession()
+        self.stops: [Dict[str, str]] = []
+        self.buses: [Dict[str, str]] = []
+        self.routes: [Dict[str, str]] = []
+        self.update()
         
     def close(self) -> None:
         return self.session.close()
@@ -14,31 +21,43 @@ class BCApi:
         if res.status // 100 != 2:
             raise RuntimeError(f"BCApi: http request failed: {res.url}")
 
-    async def get_buses(self) -> [Dict[str, str]]:
-        async with self.session.get(endpoints.BC_BUSES) as response:
-            self.validate_res_status(response)
+    def get_buses(self) -> [Dict[str, str]]:
+        return self.buses
 
-            json = await response.json()
-            return [j for j in json]
-
-    async def get_stops(self) -> [Dict[str, str]]:
-        async with self.session.get(endpoints.BC_STOPS) as response:
-            self.validate_res_status(response)
-
-            json = await response.json()
-            return [j for j in json]
+    def get_stops(self) -> [Dict[str, str]]:
+        return self.stops
 
     async def get_routes(self) -> [Dict[str, str]]:
-        async with self.session.get(endpoints.BC_ROUTES) as response:
-            self.validate_res_status(response)
-
-            json = await response.json()
-            return [j for j in json]
+        return self.routes
+        
+    async def update(self) -> None:
+        endpoint_list: [str] = [
+            endpoints.BC_BUSES,
+            endpoints.BC_ROUTES,
+            endpoints.BC_STOPS
+        ]
+        properties: [str] = [
+            "buses",
+            "routes",
+            "stops",
+        ]
+        
+        async def fetch(url: str) -> Dict[str, str]:
+            async with self.session.get(url) as response:
+                return await response.json()
+            
+        json_list = await asyncio.gather(*[fetch(url) for url in endpoint_list], return_exceptions=True)
+        
+        for i, json in enumerate(json_list):
+            setattr(self, properties[i], json)
+            
+            
 
 async def test_main() -> None:
     try: 
-        bc_wrapper: BCApi = BCApi()
-        buses: [Dict[str, str]] = await bc_wrapper.get_stops()
+        bc_wrapper: BCTransit = BCTransit()
+        await bc_wrapper.update()
+        buses: [Dict[str, str]] = bc_wrapper.get_buses()
         for bus in buses:
             print(bus)
     except RuntimeError as e:
