eb53591d1ce25d62e19d89e815c968e5d013ed57
warfarm <danz32321@gmail.com>
2024-09-21 18:49:10 -0400

    UNTESTED OCCT Serialization

--- src/crawler/occt.py
+++ src/crawler/occt.py
@@ -3,15 +3,21 @@ import asyncio
 import endpoints
 import json
 
+import logging
+import time
+from protobuf.gtfs_realtime_pb2 import FeedMessage, FeedHeader, VehiclePosition, TripUpdate, Alert
+import google.protobuf.message as pbm
+
+logger = logging.getLogger(__name__)
 
 class OCCT:
     def __init__(self):
         self.session = aiohttp.ClientSession()
-        self.VehicleData = {}
-        self.RouteData = {}
-        self.ServiceData = {}
+        self.vehicle_data = {}
+        self.trip_update_data = {}
+        self.alert_data = {}
 
-        self.protoBufData = {"Vehicle", "TripUpdate", "Alerts"}
+        self.
         self.update()
         pass
 
@@ -24,11 +30,6 @@ class OCCT:
             endpoints.OCCT_ROUTES,
             endpoints.OCCT_SERVICE_ANNOUNCEMENTS,
         ]
-        properties: [str] = [
-            "VehicleData",
-            "RouteData",
-            "ServiceData",
-        ]
 
         async def fetch(url):
             async with self.session.get(url) as response:
@@ -39,10 +40,20 @@ class OCCT:
             *[fetch(url) for url in endpoint_list], return_exceptions=True
         )
 
-        for index, json in enumerate(json_list):
-            setattr(self, properties[index], json)
+        json_list[0] = self.checkVehicles[json_list]["get_vehicles"]
 
-        self.VehicleData = self.checkVehicles(self.VehicleData)
+        vehicles_json   = json_list[0]
+        routes_json     = json_list[1]
+        service_json    = json_list[2]["get_service_announcements"]
+
+        self.vehicle_data = self.serialize_all_vehicle_positions(vehicles_json)
+        self.trip_update_data = self.serialize_all_trip_updates(vehicles_json)
+        self.alert_data = self.serialize_all_alerts(vehicles_json, service_json)
+
+        # for index, json in enumerate(json_list):
+        #     setattr(self, properties[index], json)
+
+        self.vehicle_data = self.checkVehicles(self.vehicle_data)
 
     def validate_res_status(self, res):
         if res.status // 100 != 2:
@@ -56,50 +67,104 @@ class OCCT:
         return vehicleDict
 
     def get_buses(self):
-        return self.VehicleData
+        return self.vehicle_data
 
     def get_stops(self):
-        return self.ServiceData
+        return self.alert_data
 
     def get_routes(self):
-        return self.ServiceData
+        return self.trip_update_data
 
-    # async def pollVehicles(self) -> None:
-    #     async with self.session.get(endpoints.OCCT_VEHICLES) as vehicleResponse:
-    #         self.validate_res_status(vehicleResponse)
+    async def serialize_all_vehicle_positions(self, json_argument):
+        
+        """
+        Serializes vehicle position for ONE vehicle.
+        """
+        msg = FeedMessage()
 
-    #         jsonOCCTVEHICLES = await vehicleResponse.json()
-    #         self.VehicleData = self.checkVehicles(jsonOCCTVEHICLES["get_vehicles"])
-    #         # print(self.VehicleData)
-    #     return self.VehicleData
+        # msg.header.gtfs_realtime_version = "2.0"
+        msg.header.incrementality = FeedHeader.Incrementality.FULL_DATASET
+        # TODO: do better
+        msg.header.timestamp = int(time.time())
 
-    # async def pollRoutes(self) -> None:
-    #     async with self.session.get(endpoints.OCCT_ROUTES) as routeResponse:
-    #         self.validate_res_status(routeResponse)
+        for vehicles_json in json_argument:
+            ent = msg.entity.add()
+            ent.id = "TODO"
+            self.serialize_vehicle_position(ent.vehicle, vehicles_json)
 
-    #         jsonOCCTRoutes = await routeResponse.json()
-    #         self.RouteData = jsonOCCTRoutes["get_routes"]
+        try:
+            return msg.SerializeToString()
+        except pbm.EncodeError as e:
+            logger.error(e)
+            return None
 
-    #     return self.RouteData
+    async def serialize_vehicle_position(self, output: VehiclePosition, vehicle_json):
+        output.position.latitude = vehicle_json["lat"]
+        output.position.longitude = vehicle_json["lng"]
+        output.stop_id = vehicle_json["nextStopID"]
 
-    # async def pollService(self) -> None:
-    #     async with self.session.get(
-    #         endpoints.OCCT_SERVICE_ANNOUNCEMENTS
-    #     ) as serviceResponse:
-    #         self.validate_res_status(serviceResponse)
+    async def serialize_all_trip_updates(self, json_argument):
+        
+        """
+        Serializes vehicle position for ONE vehicle.
+        """
+        msg = FeedMessage()
 
-    #         jsonOCCTService = await serviceResponse.json()
-    #         self.ServiceData = jsonOCCTService["get_service_announcements"]
+        # msg.header.gtfs_realtime_version = "2.0"
+        msg.header.incrementality = FeedHeader.Incrementality.FULL_DATASET
+        # TODO: do better
+        msg.header.timestamp = int(time.time())
 
-    #     return self.ServiceData
+        for trip_update_data in json_argument:
+            ent = msg.entity.add()
+            ent.id = "TODO"
+            self.serialize_vehicle_position(ent.trip_update, trip_update_data)
 
-    # async def pollAll(self) -> None:
-    #     await self.pollVehicles()
-    #     await self.pollRoutes()
-    #     await self.pollService()
+        try:
+            return msg.SerializeToString()
+        except pbm.EncodeError as e:
+            logger.error(e)
+            return None
 
-    # async def getVehicleInfo(self) -> None:
-    #     await self.pollVehicles()
+    async def serialize_trip_update(self, output: TripUpdate, vehicle_json):
+        output.trip.trip_id = "Bloomberg"
+        output.trip.route_id = vehicle_json["routeID"]
+        output.trip.start_time = vehicle_json["scheduleNumber"][2:10]
+        output.stop_time_update.stop_id = vehicle_json["nextStopID"]
+        output.stop_time_update.arrival.time = vehicle_json["recieveTime"]/1000
+
+    async def serialize_all_alerts(self, vehicle_json, alert_json):
+        
+        """
+        Serializes vehicle position for ONE vehicle.
+        """
+        msg = FeedMessage()
+
+        # msg.header.gtfs_realtime_version = "2.0"
+        msg.header.incrementality = FeedHeader.Incrementality.FULL_DATASET
+        # TODO: do better
+        msg.header.timestamp = int(time.time())
+
+        for alerts_data in vehicle_json:
+            ent = msg.entity.add()
+            ent.id = "TODO"
+            self.serialize_vehicle_position(ent.alert, alerts_data, alert_json)
+
+        try:
+            return msg.SerializeToString()
+        except pbm.EncodeError as e:
+            logger.error(e)
+            return None
+    
+    async def serialize_trip_update(self, output: Alert, vehicle_json, alert_json):
+        output.informed_entity.trip.trip_id = "Bloomberg"
+        output.informed_entity.route_id = vehicle_json["routeID"]
+        output.informed_entity.stop_id = vehicle_json["stopID"]
+        output.header_text.text = "Announcement"
+        output.header_text.language = "en-US"
+        output.description_text.text = alert_json["announcements"]["text"]
+        output.description_text.language = "en-US"
+
 
 
 async def main() -> None:
