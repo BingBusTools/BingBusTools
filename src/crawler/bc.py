228ce606cc15bc2b43ef60622b1ba5318c46c795
niooii <wangh3@bxscience.edu>
2024-09-21 14:24:26 -0400

    bc api wrapper

--- src/crawler/bc.py
+++ src/crawler/bc.py
@@ -1,24 +1,44 @@
 import asyncio
 import endpoints
 import aiohttp
+from typing import Dict
 
-
-class BC:
+class BCApi:
     def __init__(self):
-        pass
+        self.session = aiohttp.ClientSession()
+        
+    def validate_res_status(self, res):
+        if res.status // 100 != 2:
+            raise RuntimeError(f"BCApi: http request failed: {res.url}")
+        
+    def close(self) -> None:
+        return self.session.close()
 
-    async def poll(self) -> None:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(endpoints.BC_BUSES) as response:
-                print("Status:", response.status)
-                print("Content-type:", response.headers["content-type"])
+    async def get_buses(self) -> [Dict[str, str]]:
+        async with self.session.get(endpoints.BC_BUSES) as response:
+            self.validate_res_status(response)
 
-                html = await response.text()
-                print("Body:", html[:15], "...")
+            json = await response.json()
+            return [j for j in json]
+
+    async def get_routes(self) -> [Dict[str, str]]:
+        async with self.session.get(endpoints.BC_ROUTES) as response:
+            self.validate_res_status(response)
+
+            json = await response.json()
+            return [j for j in json]
 
 async def main() -> None:
-    a: BC = BC()
-    await a.poll()
+    try: 
+        bc_wrapper: BCApi = BCApi()
+        buses: [Bus] = await bc_wrapper.get_buses()
+        for bus in buses:
+            print(bus)
+    except e:
+        print(f"Error: {e}")
+    finally:
+        await bc_wrapper.close();
+        
 
 if __name__ == "__main__":
     asyncio.run(main())
