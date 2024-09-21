31e949dffac32ec7b9979547ea85c2e2eb5fccbf
niooii <wangh3@bxscience.edu>
2024-09-21 14:32:23 -0400

    fix code

--- src/crawler/bc.py
+++ src/crawler/bc.py
@@ -7,15 +7,22 @@ class BCApi:
     def __init__(self):
         self.session = aiohttp.ClientSession()
         
+    def close(self) -> None:
+        return self.session.close()
+        
     def validate_res_status(self, res):
         if res.status // 100 != 2:
             raise RuntimeError(f"BCApi: http request failed: {res.url}")
-        
-    def close(self) -> None:
-        return self.session.close()
 
     async def get_buses(self) -> [Dict[str, str]]:
         async with self.session.get(endpoints.BC_BUSES) as response:
+            self.validate_res_status(response)
+
+            json = await response.json()
+            return [j for j in json]
+
+    async def get_stops(self) -> [Dict[str, str]]:
+        async with self.session.get(endpoints.BC_STOPS) as response:
             self.validate_res_status(response)
 
             json = await response.json()
@@ -28,17 +35,17 @@ class BCApi:
             json = await response.json()
             return [j for j in json]
 
-async def main() -> None:
+async def test_main() -> None:
     try: 
         bc_wrapper: BCApi = BCApi()
-        buses: [Bus] = await bc_wrapper.get_buses()
+        buses: [Dict[str, str]] = await bc_wrapper.get_stops()
         for bus in buses:
             print(bus)
-    except e:
+    except RuntimeError as e:
         print(f"Error: {e}")
     finally:
         await bc_wrapper.close();
         
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(test_main())
